2b44cede55b0869eb18022193b697e33056d962e
isakibul <isakibul108@gmail.com>
2024-01-13 17:51:07 +0600

    align issue solved

--- src/page/About.jsx
+++ src/page/About.jsx
@@ -2,7 +2,7 @@ const About = () => {
     return (
         <div className="bg-stone-100 py-20 px-6 md:px-32 lg:px-40 xl:px-72 font-poppins text-justify text-xs lg:text-sm">
             <div>
-                <p className="font-righteous text-2xl lg:text-4xl font-bold text-center">Sometimes, a smaller scale of <br /> us just work.</p>
+                <p className="font-righteous text-2xl lg:text-4xl font-bold text-center">Sometimes, a smaller scale <br /> of us just work.</p>
                 <p className="my-5">In Japan due to its limited space, it's not unusual to find small shops like ours serve the finest ramen. What sets Tokio Ramen apart is the attention to detail in the art of ramen creation, and the fact that ramen is the centerpiece of our offerings. It is customary to taste the broth before enjoying the rest of the bowl to know that you have exquisite quality before you. And be sure to slurp away; we won't judge. Slurping is customary in Japan and is known to actually enhance the flavor that much more.
 
                     And about that "ramen" you've been raving about to your friends from that fusion place... yeah, you can forget about that after experiencing ours. We can post all we want about how glorious our ramen is, but you won't know until you come by and judge for yourself.</p>
