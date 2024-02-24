ae409ee099b7207be04bd11f9e33897fd2250dcc
isakibul <isakibul108@gmail.com>
2024-02-24 14:38:16 +0600

    text edited

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,7 @@
 const Contact = () => {
     return (
         <div className="bg-stone font-rubik py-2 px-6 md:px-32 lg:px-40 xl:px-72">
-            <p className="text-center font-righteous mt-20 mb-10 text-2xl md:text-4xl font-bold">Get In Touch With Us</p>
+            <p className="text-center font-righteous mt-20 mb-10 text-2xl md:text-4xl font-bold">Get In Touch</p>
             <div className="flex flex-col gap-5 font-semibold">
                 <label>Name:</label>
                 <input type="text" className="w-full py-1 px-3 focus:outline-none" />
